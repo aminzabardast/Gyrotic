d1d7a70351637e0b686ec03a86c76c95e46bfd44
Amin Zabardast <aminzabardast@gmail.com>
2025-04-18 01:18:58 +0300

    Enhance documentation for helloWorld function with detailed comments

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,9 @@
+/**
+ * Logs "Hello, world!" to the console.
+ *
+ * This function does not take any parameters and does not return any value.
+ * It is used to demonstrate a simple console log operation.
+ */
 const helloWorld = (): void => {
   console.log('Hello, world!')
 }
