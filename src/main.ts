592164ccf5daf1bb7c9547e05c03467a6f050fc4
Amin Zabardast <aminzabardast@gmail.com>
2025-04-18 01:22:04 +0300

    Add documentation comments to helloWorld function for clarity

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
+/**
+ * @description This is a simple TypeScript function that logs "Hello, world!" to the console.
+ * This is just for testing purposes.
+ */
 const helloWorld = (): void => {
   console.log('Hello, world!')
 }
