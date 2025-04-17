fb223804e4757136a398a90ea9c92d459919004d
Amin Zabardast <aminzabardast@gmail.com>
2025-04-18 01:19:46 +0300

    Remove unnecessary documentation comments from helloWorld function

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,3 @@
-/**
- * Logs "Hello, world!" to the console.
- *
- * This function does not take any parameters and does not return any value.
- * It is used to demonstrate a simple console log operation.
- */
 const helloWorld = (): void => {
   console.log('Hello, world!')
 }
